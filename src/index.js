e80cf39ee4ed9aa6e7c6141533138d6053892384
fengpeng <fengpeng@coocaa.com>
2024-06-11 15:32:40 +0800

    use webpack

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
 import chalk from "chalk";
 import Prompts from "./prompt.js";
 
---
+++ webpack.config.mjs
@@ -0,0 +1,44 @@
+//webpack.config.js
+import path from 'path';
+import url from 'url';
+
+export default {
+    mode: 'production',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(path.dirname(url.fileURLToPath(import.meta.url)), 'dist'),
+        filename: 'bundle.cjs'
+    },
+    target: 'node',
+    module: {
+        rules: [
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: [
+                  [
+                    '@babel/preset-env',
+                    {
+                      targets: {
+                        node: 'current',
+                      },
+                      useBuiltIns: 'entry',
+                      corejs: 3,
+                    },
+                  ],
+                ],
+              },
+            },
+          },
+        ],
+      },
+      resolve: {
+        fallback: {
+          fs: false,
+          path: false,
+        },
+      },
+}
